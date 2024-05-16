329942b9dc605b702c4b694c883dacad931499bf
David OC <davidherzlos@gmail.com>
2024-05-15 20:44:13 -0600

    add useful vim editor options

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,21 +1,19 @@
 -- Indicate we hava e nerd font installed.
 vim.g.have_nerd_font = true
 
--- Set <space> as the leader key
+-- Set <space> as the leader key.
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Add line nunbers for easy jumping.
 vim.opt.number = true
---vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Sync clipboard between OS and Neovim.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
 vim.opt.clipboard = 'unnamedplus'
 
--- Show mode in status line.
-vim.opt.showmode = true
+-- Show mode in status line off, we have a custom status line.
+vim.opt.showmode = false
 
 -- Set highlight on search, but clear on pressing <Esc> in normal mode.
 vim.opt.hlsearch = true
@@ -23,3 +21,26 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
+
+-- Enable 24 bit colours in the terminal.
+vim.opt.termguicolors = true
+
+-- Preview substitutions when typing.
+vim.opt.inccommand = 'split'
+
+-- Display some whitespace characters.
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- Decrease update time
+vim.opt.updatetime = 250
+
+-- Decrease mapped sequence wait time
+-- Displays which-key popup sooner
+vim.opt.timeoutlen = 300
+
+-- Hide cmdline when it is not being used.
+vim.opt.cmdheight = 0
+
+-- Show which line your cursor is on
+vim.opt.cursorline = true
