baa8107b106fa6fb318a45a6dd1628511c3f63a7
David OC <davidherzlos@gmail.com>
2024-06-08 16:18:42 -0600

    Update icons for signs config

--- lua/plugins/editing.lua
+++ lua/plugins/editing.lua
@@ -1,9 +1,32 @@
 return {
   -- "gc" to comment visual regions/lines
-  { 
-    'numToStr/Comment.nvim', 
-    opts = {} 
+  {
+    'numToStr/Comment.nvim',
+    opts = {}
   },
   -- Highlight todo, notes, etc in comments
-  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = true } },
+  { 'folke/todo-comments.nvim',
+    event = 'VimEnter',
+    dependencies = {
+      'nvim-lua/plenary.nvim'
+    },
+    opts =
+    {
+      signs = true,
+      keywords = {
+        FIX = {
+          icon = " ",
+          color = "error", -- It accepts also hex values.
+          alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, -- Map this keyword too.
+        },
+        TODO = { icon = " ", color = "info" },
+        HACK = { icon = " ", color = "warning" },
+        WARN = { icon = " ", color = "warning", alt = { "WARNING" } },
+        PERF = { icon = " ", alt = { "PERFORMANCE" } },
+        NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
+        TEST = { icon = " ", color = "info", alt = { "PASSED", "FAILED" } },
+      },
+    }
+
+  },
 }
