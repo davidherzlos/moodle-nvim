1cbbbee8178add2cfcd6ce566f1eeb8735dd407d
David OC <davidherzlos@gmail.com>
2024-06-25 19:45:35 -0600

    clean and refine config for completions

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,83 +1,106 @@
 return {
   {
-    'hrsh7th/nvim-cmp',      -- Completion engine.
+    'hrsh7th/nvim-cmp',                      -- Completion engine.
     dependencies = {
-      'rafamadriz/friendly-snippets', -- VSCode snippets.
-      'saadparwaiz1/cmp_luasnip', -- LuaSnip completion source for nvim-cmp.
-      'L3MON4D3/LuaSnip',      -- Snippets expansion engine.
-      'hrsh7th/cmp-nvim-lsp',  -- Completion source for builting lsp's.
+      'rafamadriz/friendly-snippets',        -- VSCode generic snippets.
+      'ManuelGil/vscode-moodle-snippets',    -- VSCode snippets for Moodle Development.
+      'saadparwaiz1/cmp_luasnip',            -- LuaSnip completion source for nvim-cmp.
+      {
+        "L3MON4D3/LuaSnip",                  -- Snippets expansion engine.
+        version = "v2.*",
+	build = "make install_jsregexp"
+      },
+      'doxnit/cmp-luasnip-choice',           -- Autochoice completion node.
+      'hrsh7th/cmp-nvim-lsp',                -- Completion source for builting lsp's.
+      'onsails/lspkind.nvim'                 -- Some pretty icons for lsp completions.
     },
 
-    -- 'neovim/nvim-lspconfig', --tc
-    -- 'hrsh7th/cmp-buffer',    --tc
-    -- 'hrsh7th/cmp-path',      --tc
-    -- 'hrsh7th/cmp-cmdline',   --tc
-    config = function()
-      local cmp = require'cmp'
+    -- TODO: Review these potential sources for completions:
+    -- 'neovim/nvim-lspconfig', 'hrsh7th/cmp-buffer',
+    -- 'hrsh7th/cmp-path',
+    -- 'hrsh7th/cmp-cmdline',
 
-      require("luasnip.loaders.from_vscode").lazy_load()
+    config = function()
+      -- NOTE: These are convenient options for the menu, but they need to be tested.
+      -- vim.opt.completeopt = { "menu", "menuone", "preview", "noselect" }
+
+      -- Require some variables for further configuration.
+      local cmp = require'cmp'
+      local lspkind = require('lspkind')
+      lspkind.init()
 
       cmp.setup({
+
+        -- Configure some icons to be shown on the completion list.
+        formatting = {
+          format = lspkind.cmp_format({
+            preset = 'codicons', -- VsCode like icon.
+            -- These are some custom icon overridings.
+            symbol_map = {
+               Text = "󰉿",
+               Function = "󰡱",
+               Constructor = "󰒕",
+               Field = "󰜢",
+               Variable = "󱕂",
+               Property = "󰜢",
+               Enum = "",
+               Snippet = "",
+               File = "",
+               Folder = "󰉋",
+               EnumMember = "",
+               Constant = "󰏿",
+               Struct = "󰙅",
+               Event = "",
+               Operator = "󰆕",
+               TypeParameter = "",
+            },
+            mode = 'symbol_text',
+            maxwidth = 35,
+            ellipsis_char = '...',
+            show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+          })
+        },
+
+        -- Configure snippet expansion behaviour for nvim-cmp.
         snippet = {
           expand = function(args)
-            require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+            require('luasnip').lsp_expand(args.body)
           end,
         },
+
+        -- Define some UI options.
         window = {
           completion = cmp.config.window.bordered(),
           documentation = cmp.config.window.bordered(),
         },
+
+        -- Define some mappings to interact with the completion list.
         mapping = cmp.mapping.preset.insert({
           ['<C-b>'] = cmp.mapping.scroll_docs(-4),
           ['<C-f>'] = cmp.mapping.scroll_docs(4),
           ['<C-Space>'] = cmp.mapping.complete(),
           ['<C-e>'] = cmp.mapping.abort(),
-          ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
         }),
-        sources = cmp.config.sources({
-          { name = 'luasnip' }, -- For luasnip users.
-          { name = 'nvim_lsp' },
-        }, {
-          { name = 'buffer' },
-        })
-      })
 
-      -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-      -- cmp.setup.cmdline({ '/', '?' }, {
-      --   mapping = cmp.mapping.preset.cmdline(),
-      --   sources = {
-      --     { name = 'buffer' }
-      --   }
-      -- })
-
-      -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-      -- cmp.setup.cmdline(':', {
-      --   mapping = cmp.mapping.preset.cmdline(),
-      --   sources = cmp.config.sources({
-      --     { name = 'path' }
-      --   }, {
-      --     { name = 'cmdline' }
-      --   }),
-      --   matching = { disallow_symbol_nonprefix_matching = false }
-      -- })
+        -- Tell nvim-cmp the sources for the completion list.
+        sources = cmp.config.sources({ { name = 'luasnip' }, { name = 'nvim_lsp' } }, { { name = 'buffer' } }) })
 
       -- Broadcast capabilities for each lsp we have installed.
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-      -- Bash LSP.
       require('lspconfig').bashls.setup {
         capabilities = capabilities
       }
-
-      -- Lua LSP.
       require('lspconfig').lua_ls.setup {
         capabilities = capabilities
       }
-
-      -- PHP LSP.
       require('lspconfig').intelephense.setup {
         capabilities = capabilities
       }
+
+      -- Finally load some generic and Moodle specific vscode snippets packages.
+      require("luasnip.loaders.from_vscode").lazy_load()
+
     end
   },
 }
