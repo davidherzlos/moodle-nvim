ea5f3114d15c546fbf87a1ff76d11f7e8f79b06a
David OC <davidherzlos@gmail.com>
2024-11-24 18:39:54 -0600

    Add vim Fugitive plugin and tweak config for Gitsigns

--- lua/plugins/version-control.lua
+++ lua/plugins/version-control.lua
@@ -1,5 +1,9 @@
 return {
   {
+    'tpope/vim-fugitive',
+    enabled = true
+  },
+  {
     'lewis6991/gitsigns.nvim',
     opts = {
       signs = {
@@ -29,40 +33,6 @@ return {
           vim.keymap.set(mode, l, r, opts)
         end
 
-        -- Navigation between unstaged Hunks.
-        map('n', ']h', function()
-          if vim.wo.diff then
-            vim.cmd.normal({']h', bang = true})
-          else
-            gitsigns.nav_hunk('next')
-          end
-        end, { desc = 'Git: Next Unstaged Hunk' })
-
-        map('n', '[h', function()
-          if vim.wo.diff then
-            vim.cmd.normal({'[h', bang = true})
-          else
-            gitsigns.nav_hunk('prev')
-          end
-        end, { desc = 'Git: Previous Unstaged Hunk'})
-
-        -- Navigation between staged Hunks.
-        map('n', ']]h', function()
-          if vim.wo.diff then
-            vim.cmd.normal({']]h', bang = true})
-          else
-            gitsigns.nav_hunk('next', { target = 'staged' })
-          end
-        end, { desc = 'Git: Next Staged Hunk' })
-
-        map('n', '[[h', function()
-          if vim.wo.diff then
-            vim.cmd.normal({'[[h', bang = true})
-          else
-            gitsigns.nav_hunk('prev', { target = 'staged' })
-          end
-        end, { desc = 'Git: Previous Staged Hunk'})
-
         map('n', '<leader>gp', function()
           gitsigns.toggle_linehl()
         end, { desc = 'Git: [G]it [P]review' })
@@ -71,14 +41,11 @@ return {
           gitsigns.toggle_deleted()
         end, { desc = 'Git: [G]it [D]eleted' })
 
-        map('n', '<leader>hq', function() gitsigns.setqflist('all') end, { desc = 'Git: [H]unk [Q]quickfixList' })
-        map('n', '<leader>hl', function() gitsigns.setqflist(0) end, { desc = 'Git: [H]unk [L]ocation List' })
-
-        -- Buffer Hunks.
-        map('n', '<leader>hr', gitsigns.reset_hunk, { desc = ' Git: [H]unk [R]estore' })
-        map('n', '<leader>hs', gitsigns.stage_hunk, { desc = 'Git: [H]unk [S]tage' })
-        map('n', '<leader>hu', gitsigns.undo_stage_hunk, { desc = 'Git: [H]unk [U]nstage' })
+        map('n', '<leader>gw', "<cmd>Git difftool<cr>", { desc = 'Git: [G]it [W]ork hunks' })
+        map('n', '<leader>gi', "<cmd>Git difftool --staged<cr>", { desc = 'Git: [G]it [I]ndex hunks' })
         map('n', '<leader>gb', gitsigns.blame, { desc = 'Gitsigns: [G]it [B]lame' })
+        map('n', '<leader>hr', gitsigns.reset_hunk, { desc = ' Git: [H]unk [R]estore' })
+        map('n', '<leader>hs', gitsigns.stage_hunk, { desc = 'Git: [H]unk toggle [S]tage' })
       end
     },
   },
