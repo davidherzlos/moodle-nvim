284f5fdc58528d7296386d655ebee9fb48b10f33
David OC <davidherzlos@gmail.com>
2025-05-16 01:25:59 -0600

    feat: change themes selection and highlights for git diff view and dap
    - removed rose-pine color scheme
    - added nightfox(dawnfox) color scheme
    - added ayu color scheme

--- lua/plugins/color-schemes.lua
+++ lua/plugins/color-schemes.lua
@@ -1,24 +1,12 @@
--- TODO: Refactor this functions to use correctly the APIS.
--- Customize signs for Debugging.
-vim.api.nvim_set_hl(0, 'DapBreakPoint', { ctermbg = 0, fg = '#db4b4b', bg = '#2c0f0f' })
-vim.api.nvim_set_hl(0, 'DapLogPoint', { ctermbg = 0, fg = '#e0af68', bg = '#2d2315' })
-vim.api.nvim_set_hl(0, 'DapStopped', { ctermbg = 0, fg = '#1abc9c', bg = '#05261f' })
-vim.fn.sign_define("DapBreakpoint", { text = " ", texthl = "DapBreakPoint", linehl = "DapBreakPoint", numhl = "DapBreakPoint"})
-vim.fn.sign_define("DapLogPoint", { text = " ", texthl = "DapLogPoint", linehl = "DapLogPoint", numhl = "DapLogPoint"})
-vim.fn.sign_define("DapStopped", { text = " ", texthl = "DapStopped", linehl = "DapStopped", numhl = "DapStopped"})
-
 return {
   {
     "folke/tokyonight.nvim",
     enabled = vim.g.default_colorscheme == 'tokyonight',
     lazy = true,
-    priority = 1000,
     config = function ()
       require('tokyonight').setup({
         style = 'night',
-        transparent = true,
         styles = {
-          floats = "transparent",
           comments = {
             italic = false,
           },
@@ -26,54 +14,15 @@ return {
             italic = false,
             bold = true,
           },
+          floats = 'transparent',
         },
-        dime_inactive = false,
-        on_colors = function (colors)
-        end,
-        on_highlights = function(hl, c)
-          local prompt = c.bg
-          hl.TelescopeNormal = {
-            bg = c.bg,
-            fg = c.fg,
-          }
-          hl.TelescopeBorder = {
-            bg = c.bg,
-            fg = c.bg,
-          }
-          hl.TelescopePromptNormal = {
-            bg = prompt,
-          }
-          hl.TelescopePromptBorder = {
-            bg = prompt,
-            fg = prompt,
-          }
-          hl.TelescopePromptTitle = {
-            bg = c.bg_highlight,
-            fg = c.fg,
-          }
-          hl.TelescopePreviewTitle = {
-            bg = c.bg_highlight,
-            fg = c.fg,
-          }
-          hl.TelescopeResultsTitle = {
-            bg = c.bg_highlight,
-            fg = c.fg,
-          }
-          hl.CursorLine = {
-            bg = c.bg
-          }
-          hl.MiniIndentscopeSymbol = {
-            fg = "#515a81"
-          }
-        end,
+        on_highlights = function (highlights, colors)
+          -- Dap highlights.
+          highlights.DapBreakPoint = { fg = colors.error, }
+          highlights.DapLogPoint = { fg = colors.terminal.yellow_bright, }
+          highlights.DapStopped = { fg = colors.fg, }
+        end
       })
-      -- Customize highlights for GitSigns.
-      vim.api.nvim_set_hl(0, 'GitSignsAddLn', { bg = '#1a282e' })
-      vim.api.nvim_set_hl(0, 'GitSignsChangeLn', { bg = '#1a282e' })
-      vim.api.nvim_set_hl(0, 'GitSignsStagedAddLn', { bg = '#132a40' })
-      vim.api.nvim_set_hl(0, 'GitSignsStagedChangeLn', { bg = '#132a40' })
-      vim.api.nvim_set_hl(0, 'GitSignsDeleteVirtLn', { bg = '#301014' })
-      vim.api.nvim_set_hl(0, 'GitSignsStagedChangedeleteLn', { bg = '#301026' })
     end
   },
   {
@@ -84,16 +33,28 @@ return {
     opts = {
       transparent = {
         enabled = true,
+        pmenu = false,
+        normal = false,
+        normalfloat = true,
         whichkey = false,
+        telescope = true,
+        lazy = true,
+      },
+      error_lens = {
+        text = true,
+        symbol = false,
       },
     },
     config = function(_, opts)
       opts.on_highlights = function (colors, color)
-        ---@type solarized.highlights
         local groups = {
-            GitSignsStagedAddLn = { fg = colors.cyan, bg = colors.base2 },
-            GitSignsStagedChangeLn = { fg = colors.yellow, bg = colors.base2 },
-            GitSignsStagedChangedeleteLn = { fg = colors.red, bg = colors.base2 }
+          LineNr = { fg = colors.base1 },
+          diffAdded = { bg = "#e7eddb", fg = colors.git_add },
+          diffRemoved = { bg = "#f8e2d9", fg = colors.git_delete },
+            -- Dap highlights.
+            DapBreakPoint = { bg = colors.base3, fg = colors.red, },
+            DapLogPoint = { bg = colors.base3, fg = colors.yellow, },
+            DapStopped = { bg = colors.base3, fg = colors.blue, },
         }
         return groups
       end
@@ -102,58 +63,44 @@ return {
       vim.o.background = 'light'
     end,
   },
-  --[[
-base4............................... = "#fbf3db" 
-mix_yellow.......................... = "#364725" 
-diag_error.......................... = "#DC322F" 
-git_delete.......................... = "#DC322F" 
-git_modify.......................... = "#B58900" 
-base2............................... = "#eee8d5"  background highlight (light)
-base00.............................. = "#657B83" foreground (light)
-base3............................... = "#fdf6e3" background (light)
-mix_violet.......................... = "#204060" 
-violet.............................. = "#6C71C4" 
-orange.............................. = "#CB4B16" 
-red................................. = "#DC322F" 
-base03.............................. = "#002B36" background (dark)
-base02.............................. = "#073642" background highlight (dark)
-mix_cyan............................ = "#0C4E53" 
-mix_magenta......................... = "#3F2E4C" 
-magenta............................. = "#D33682" 
-diag_ok............................. = "#859900" 
-mix_base1........................... = "#2C4E56" 
-mix_orange.......................... = "#3C342C" 
-cyan................................ = "#2AA198" 
-yellow.............................. = "#B58900" 
-diag_hint........................... = "#268BD2" 
-mix_red............................. = "#422D33" 
-mix_base01.......................... = "#CCCDC2" 
-mix_green........................... = "#274C25" 
-green............................... = "#859900" 
-git_add............................. = "#268BD2" 
-diag_info........................... = "#268BD2" 
-base01.............................. = "#586E75" comments (dark)
-base1............................... = "#93A1A1" comments (light)
-base0............................... = "#839496" foreground (dark)
-diag_warning........................ = "#B58900" 
-base04.............................. = "#002731" 
-blue................................ = "#268BD2" 
-mix_blue............................ = "#0B4764" 
-    ]]--
   {
-    "rose-pine/neovim",
-    name = "rose-pine",
-    enabled = vim.g.default_colorscheme == 'rose-pine',
+    'Shatur/neovim-ayu',
+    enabled = vim.g.default_colorscheme == 'ayu',
     lazy = true,
+    priority = 1000,
     config = function ()
-      require('rose-pine').setup({
-        variant = "dawn",
-        dark_variant = "dawn",
-        styles = {
-          italic = false,
-          transparent = true,
-        }
+      local colors = require('ayu.colors')
+      colors.generate(true) -- Pass `true` to enable mirage
+      require('ayu').setup({
+          overrides = {
+            Comment = { fg = colors.comment },
+            -- Dap highlights.
+            DapBreakPoint = { fg = colors.red, },
+            DapLogPoint = { fg = colors.yellow, },
+            DapStopped = { fg = colors.entity, },
+          }
       })
     end
-  }
+  },
+  {
+    "EdenEast/nightfox.nvim",
+    enabled = vim.g.default_colorscheme == 'dawnfox',
+    lazy = true,
+    priority = 1000,
+    config = function ()
+      require('nightfox').setup({
+        groups = {
+          dawnfox = {
+            diffAdded = { bg = "palette.sel0"},
+            diffRemoved = { bg = "#ecd7d6"},
+            MiniIndentscopeSymbol = { fg = "palette.magenta.dim"},
+            -- Dap highlights.
+            DapBreakPoint = { fg = "palette.red.dim", },
+            DapLogPoint = { fg = "palette.yellow.dim", },
+            DapStopped = { fg = "palette.blue.dim", },
+          }
+        },
+      })
+    end
+  },
 }
