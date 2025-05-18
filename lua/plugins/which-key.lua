c8a205619888c83898fce38be266ff8931985ba2
David OC <davidherzlos@gmail.com>
2025-05-17 23:53:00 -0600

    feat: increase delay time for which key and customize some opts

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -5,17 +5,20 @@ return {
     event = "VeryLazy",
     opts = {
       preset = "classic",
+      delay = function (ctx)
+        return 700
+      end,
       icons = {
         group = (vim.g.fallback_icons_enabled and "+") or "",
         rules = false,
-        separator = "-",
+        separator = ">",
       },
     },
     keys = {
       {
         "<leader>?",
         function()
-          require("which-key").show({ global = false })
+          require("which-key").show({ global = true })
         end,
         desc = "Buffer Local Keymaps (which-key)",
       },
