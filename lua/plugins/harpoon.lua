5a9ca0a81c5000fae5ea840dfbfbc32be6d57dcf
David OC <davidherzlos@gmail.com>
2025-05-18 00:26:37 -0600

    refactor: re-arrange harpoon keymaps and remove unused line

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -10,16 +10,13 @@ return {
       local harpoon = require("harpoon")
       harpoon:setup()
 
-      -- Use Telescope as the ui for the list.
-      local conf = require("telescope.config").values
-
       -- Define custom keymaps.
-      vim.keymap.set("n", "<C-p>", function() harpoon:list():add() end, { noremap = true, desc = "Harpoon: Pin"})
-      vim.keymap.set("n", "<C-h>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { noremap = true, desc = "Harpoon: List" })
-      vim.keymap.set("n", "<M-h>", function() harpoon:list():select(1) end, { desc = "Harpoon: Buffer 1" })
-      vim.keymap.set("n", "<M-j>", function() harpoon:list():select(2) end, { desc = "Harpoon: Buffer 2" })
-      vim.keymap.set("n", "<M-k>", function() harpoon:list():select(3) end, { desc = "Harpoon: Buffer 3" })
-      vim.keymap.set("n", "<M-l>", function() harpoon:list():select(4) end, { desc = "Harpoon: Buffer 4" })
+      vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end, { desc = "Harpoon: Buffer 1" })
+      vim.keymap.set("n", "<C-j>", function() harpoon:list():select(2) end, { desc = "Harpoon: Buffer 2" })
+      vim.keymap.set("n", "<C-k>", function() harpoon:list():select(3) end, { desc = "Harpoon: Buffer 3" })
+      vim.keymap.set("n", "<C-l>", function() harpoon:list():select(4) end, { desc = "Harpoon: Buffer 4" })
+      vim.keymap.set("n", "<C-a>", function() harpoon:list():add() end, { desc = "Harpoon: Pin"})
+      vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Harpoon: list" })
       end
       }
     }
