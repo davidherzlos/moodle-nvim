3160f5f6cc6a5045c07eb8ca255f76eb8bd92e92
David OC <davidherzlos@gmail.com>
2025-05-18 21:43:11 -0600

    dep: remove keymaps for blocking arrow keys

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,11 +1,5 @@
 --[[General]]--
 
--- Disable arrow keys in normal mode
-vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
-vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
-vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
-vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
-
 -- Clear highlight search when pressing <Esc> in normal mode.
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>', { noremap = true })
 
