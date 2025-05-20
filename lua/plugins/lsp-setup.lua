bbd6b1e7222ce0c877917fcf4eeda58e91a53023
David OC <davidherzlos@gmail.com>
2025-05-19 23:59:16 -0600

    refactor: tidy config for lsp-setup

--- lua/plugins/lsp-setup.lua
+++ lua/plugins/lsp-setup.lua
@@ -1,67 +1,103 @@
 -- Configure the communication between neovim and the LSPs.
 return {
   {
-    "neovim/nvim-lspconfig",
+    'neovim/nvim-lspconfig',
     dependencies = {
-      "williamboman/mason.nvim", -- Mason package manager for LSPs. DAPs, Linters, Formatters, etc.
-      "williamboman/mason-lspconfig.nvim", -- Ensure all specified Language Servers (LSPs) are installed.
+      "williamboman/mason.nvim", -- Package manager for LSPs. DAPs, Linters, Formatters, etc.
+      "williamboman/mason-lspconfig.nvim", -- Ensure all specified tools are installed.
     },
-    config = function()
-      -- First setup mason,
-      require("mason").setup()
+    opts = {
+      servers = {
+        -- Setup LSP for bash programming.
+        bashls = {},
 
-      -- Then specify servers that that i want to install.
-      require("mason-lspconfig").setup({
-        ensure_installed = { "bashls", "lua_ls", "intelephense", "biome", "ts_ls" }
-      })
+        -- Setup LSP for lua programming.
+        -- https://luals.github.io/wiki/settings/
+        lua_ls = {
+          on_init = function(client)
+            if client.workspace_folders then
+              local path = client.workspace_folders[1].name
+              if vim.loop.fs_stat(path..'/.luarc.json') or vim.loop.fs_stat(path..'/.luarc.jsonc') then
+                return
+              end
+            end
+            client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+              runtime = {
+                version = 'LuaJIT'
+              },
+              workspace = {
+                checkThirdParty = false,
+                library = {
+                  vim.env.VIMRUNTIME,
+                  '~/.local/share/nvim/lazy/solarized.nvim',
+                }
+              }
+            })
+          end,
+          settings = {
+            Lua = {
+              hint = {
+                enable = false,
+              },
+              signatureHelp = {
+                enable = false,
+              },
+            }
+          }
+        },
 
-      -- Configure the language servers.
-      local lspconfig = require("lspconfig")
-
-      -- Setup for bash programming.
-      lspconfig.bashls.setup({})
-
-      -- Setup for lua programming.
-      lspconfig.lua_ls.setup({})
-
-      -- Setup for php programming.
-      lspconfig.intelephense.setup({
-        settings = {
-          intelephense = {
-            files = {
-              maxSize = {
-                type = "number",
-                default = 1500000,
+        -- Setup LSP for php programming.
+        -- https://github.com/bmewburn/vscode-intelephense/blob/ff4677f07cb47911852fb265fe16742a312bd9b7/package.json
+        intelephense = {
+          settings = {
+            intelephense = {
+              files = {
+                maxSize = {
+                  type = "number",
+                  default = 1500000,
+                },
               },
             },
-          },
-        }
-      })
+          }
+        },
 
-      -- Setup for nodejs programming.
-      lspconfig.biome.setup({
-        filetypes = {
-          "javascript",
-          "javascriptreact",
-          "javascript.jsx", -- Experimental.
-          "json",
-          "jsonc",
-          "astro",
-          "svelte",
-          "vue",
-          "css"
-        }
-      })
+        -- Setup LSP for nodejs programming.
+        biome = {
+          filetypes = {
+            "javascript",
+            "javascriptreact",
+            "javascript.jsx", -- Experimental.
+            "json",
+            "jsonc",
+            "astro",
+            "svelte",
+            "vue",
+            "css"
+          }
+        },
 
-      -- Setup for typescript programming.
-      lspconfig.ts_ls.setup({
-        {
-          "typescript",
-          "typescriptreact",
-          "typescript.tsx"
+        -- Setup LSP for typescript programming.
+        ts_ls = {
+          {
+            "typescript",
+            "typescriptreact",
+            "typescript.tsx",
+          }
+        },
+      }
+    },
+    config = function(_, opts)
+      require("mason").setup()
+      require("mason-lspconfig").setup({
+        automatic_enable = false,
+        ensure_installed = {
+          "bashls",
+          "lua_ls",
+          "intelephense",
+          "biome",
+          "ts_ls",
         }
       })
     end
   },
 }
-
