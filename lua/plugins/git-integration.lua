0306fa5ef2a24c607eadb2c8da1d3303e66e875b
David OC <davidherzlos@gmail.com>
2025-05-30 17:51:50 -0600

    refactor: tidy git integration file

--- lua/plugins/git-integration.lua
+++ lua/plugins/git-integration.lua
@@ -1,29 +1,36 @@
 return {
+  -- MiniDiff to highlight signs with git changes.
+  {
+    'echasnovski/mini.nvim',
+    config = function ()
+      vim.keymap.set('n', '<leader>Gp', function ()
+        vim.cmd('MiniDiff.toggle_overlay()')
+      end, { desc = '[G]it [p]review worktree'})
+    end
+  },
+  -- Vim fugitive to integrate a wide range of git operations on vim cmd.
   {
     'tpope/vim-fugitive',
-    dependencies = {
-      'echasnovski/mini.nvim',
-    },
     config = function ()
-      -- Use Minidiff to preview changes on the working tree, index changes are not supported yet.
       require('mini.diff').setup()
-      vim.keymap.set('n', '<leader>Gp', '<cmd>lua MiniDiff.toggle_overlay()<CR>', { silent = true, desc = '[G]it [p]review worktree' })
 
       -- Add Keymaps to open diff split views for changes and conflicts.
       vim.keymap.set('n', '<leader>Gwd', function ()
         vim.cmd('Gvdiffsplit')
         vim.opt.laststatus = 3
       end, { desc = '[G]it [w]orktree [d]iffsplit' })
+
       vim.keymap.set('n', '<leader>Gid', function ()
         vim.cmd('Gvdiffsplit HEAD')
         vim.opt.laststatus = 3
       end, { desc = '[G]it [i]ndex [d]iffsplit' })
+
       vim.keymap.set('n', '<leader>Gmd', function ()
        vim.cmd('Gvdiffsplit!')
         vim.opt.laststatus = 3
       end, { desc = '[G]it [m]erge [d]iffsplit' })
 
-      -- Keymaps to open qflists and loclists of changes to the worktree.
+      -- Add keymaps to open qflists and loclists of changes to the worktree.
       vim.keymap.set('n', '<leader>Gwc', function()
         vim.cmd('Git difftool')
       end, { noremap = true, silent = true, desc = '[G]it [w]orktree [c]open' })
@@ -33,7 +40,7 @@ return {
         vim.fn.setloclist(0, vim.fn.getqflist()) vim.cmd("lopen")
       end, { noremap = true, silent = true, desc = '[G]it [w]orktree [l]open' })
 
-      -- Keymaps to open qflists and loclists of changes to the index.
+      -- Add keymaps to open qflists and loclists of changes to the index.
       vim.keymap.set('n', '<leader>Gic', function()
         vim.cmd('Git difftool --staged')
       end, { noremap = true, silent = true, desc = '[G]it [i]ndex [c]open' })
@@ -52,9 +59,9 @@ return {
         vim.cmd('Git mergetool %') vim.cmd("cclose")
         vim.fn.setloclist(0, vim.fn.getqflist()) vim.cmd("lopen")
       end, { noremap = true, silent = true, desc = '[G]it [m]erge [l]open' })
-
     end
   },
+  -- Lazygit foe more visual git users.
   {
     "kdheepak/lazygit.nvim",
     cmd = {
@@ -64,7 +71,6 @@ return {
       "LazyGitFilter",
       "LazyGitFilterCurrentFile",
     },
-    -- optional for floating window border decoration
     dependencies = {
       "nvim-lua/plenary.nvim",
     },
