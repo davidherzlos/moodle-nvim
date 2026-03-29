ad09f796bf29e7bf03f3755b3c20870a33d54ca4
David OC <davidherzlos@gmail.com>
2026-03-28 22:29:06 -0600

    dep: remove noice.nvim plugin spec

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -24,47 +24,6 @@ return {
       },
     }
   },
-  -- Some UI improvements for the editor.
-  {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    opts = {
-      cmdline = {
-        view = 'cmdline',
-        format = {
-          cmdline = { pattern = "^:", icon = "", lang = "vim" },
-          search_down = { kind = "search", pattern = "^/", icon = "", lang = "regex" },
-          search_up = { kind = "search", pattern = "^%?", icon =  "", lang = "regex" },
-          filter = { pattern = "^:%s*!", icon = "", lang = "bash" },
-          lua = { pattern = { "^:%s*lua%s+", "^:%s*lua%s*=%s*", "^:%s*=%s*" }, icon = "", lang = "lua" },
-          help = { pattern = "^:%s*he?l?p?%s+", icon = "󰋖" },
-          input = { view = "cmdline_input", icon = "󰥻 " },
-        },
-      },
-      lsp = {
-        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-        override = {
-          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-          ["vim.lsp.util.stylize_markdown"] = true,
-        },
-        hover = {
-          enabled = false
-        },
-        signature = {
-          enabled = false
-        }
-      },
-      -- you can enable a preset for easier configuration
-      presets = {
-        bottom_search = false, -- use a classic bottom cmdline for search
-        command_palette = true, -- position the cmdline and popupmenu together
-        long_message_to_split = true, -- long messages will be sent to a split
-        inc_rename = false, -- enables an input dialog for inc-rename.nvim
-        lsp_doc_border = false, -- add a border to hover docs and signature help
-      },
-    },
-    dependencies = { "MunifTanjim/nui.nvim" }
-  },
   -- Diagnostics inline.
   {
     "rachartier/tiny-inline-diagnostic.nvim",
