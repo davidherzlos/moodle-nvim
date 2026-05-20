22ccab6c26aa9a1f76143ad0f78906dbd99829f1
David OC <davidherzlos@gmail.com>
2026-05-19 18:05:09 -0600

    chore(keymaps): update keymaps for navigation(harpoon)

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -11,12 +11,12 @@ return {
       harpoon:setup()
 
       -- Define custom keymaps.
-      vim.keymap.set("n", "<leader>gh", function() harpoon:list():select(1) end, { desc = "Harpoon: Buffer 1" })
-      vim.keymap.set("n", "<leader>gj", function() harpoon:list():select(2) end, { desc = "Harpoon: Buffer 2" })
-      vim.keymap.set("n", "<leader>gk", function() harpoon:list():select(3) end, { desc = "Harpoon: Buffer 3" })
-      vim.keymap.set("n", "<leader>gl", function() harpoon:list():select(4) end, { desc = "Harpoon: Buffer 4" })
-      vim.keymap.set("n", "<leader>ha", function() harpoon:list():add() end, { desc = "Harpoon: add"})
-      vim.keymap.set("n", "<leader>hl", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Harpoon: list" })
+      vim.keymap.set("n", "<leader>nh", function() harpoon:list():select(1) end, { desc = "Nav to h (1)" })
+      vim.keymap.set("n", "<leader>nj", function() harpoon:list():select(2) end, { desc = "Nav to j (2)" })
+      vim.keymap.set("n", "<leader>nk", function() harpoon:list():select(3) end, { desc = "Nav to k (3)" })
+      vim.keymap.set("n", "<leader>nl", function() harpoon:list():select(4) end, { desc = "Nav to l (4)" })
+      vim.keymap.set("n", "<leader>na", function() harpoon:list():add() end, { desc = "Add to nav list" })
+      vim.keymap.set("n", "<leader>nn", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Nav list" })
       end
       }
     }
