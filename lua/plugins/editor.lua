b2b1f507429ccf6a0bfd529102b8b425cd2d6812
David OC <davidherzlos@gmail.com>
2026-06-08 05:16:35 -0600

    add specs to customize the tabline and signature help popups

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,4 +1,57 @@
 return {
+  -- Show signature help and parameter hints.
+  {
+    "ray-x/lsp_signature.nvim",
+    event = "InsertEnter",
+    opts = {
+      debug = false, -- set to true to enable debug logging
+      log_path = vim.fn.stdpath("log") .. "/lsp_signature.log", -- log dir when debug is true
+      -- default is  ~/.cache/nvim/lsp_signature.log
+      verbose = false, -- show debug line number
+      -- If you want to hook lspsaga or other signature handler, pls set to false
+      doc_lines = 0, -- will show two lines of comment/doc(if there are more than two lines in doc, will be truncated);
+      -- set to 0 if you DO NOT want any API comments be shown
+      -- This setting only take effect in insert mode, it does not affect signature help in normal
+      -- mode, 10 by default
+      wrap = true, -- allow doc/signature text wrap inside floating_window, useful if your lsp return doc/sig is too long
+      floating_window = false, -- show hint in a floating window, set to false for virtual text only mode
+      floating_window_above_cur_line = true, -- try to place the floating above the current line when possible Note:
+      -- will set to true when fully tested, set to false will use whichever side has more space
+      -- this setting will be helpful if you do not want the PUM and floating win overlap
+      close_timeout = 4000, -- close floating window after ms when laster parameter is entered
+      fix_pos = true,  -- set to true, the floating window will not auto-close until finish all parameters
+      hint_enable = true,
+      hint_prefix = " ",
+      hint_scheme = "DiagnosticVirtualTextInfo",
+      hint_inline = false,
+      handler_opts = {
+        border = "rounded"   -- double, rounded, single, shadow, none, or a table of borders
+      },
+      zindex = 200, -- by default it will be on top of all floating windows, set to <= 50 send it to bottom
+      padding = '', -- character to pad on left and right of signature can be ' ', or '|'  etc
+      toggle_key_flip_floatwin_setting = false, -- true: toggle floating_windows: true|false setting after toggle key pressed
+    },
+  },
+  -- Tabline customization.
+  {
+    'nanozuki/tabby.nvim',
+    config = function()
+      require('tabby').setup({
+        preset = 'active_wins_at_tail',
+        option = {
+          nerdfont = true,              -- whether use nerdfont
+          tab_name = {
+            name_fallback = function(tabid)
+              return ''
+            end,
+          },
+          buf_name = {
+            mode = 'tail', -- or 'relative', 'tail', 'shorten'
+          },
+        },
+      })
+    end,
+  },
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim',
     event = 'VimEnter',
